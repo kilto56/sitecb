767a358c09fe2391bd7c92b167c924d3066c8109
basileusesport <kill.maldeme@gmail.com>
2024-04-06 14:07:21 +0200

    mep système black light online

--- v2/sources/js/changestyle.js
+++ v2/sources/js/changestyle.js
@@ -1,17 +1,22 @@
-let changeStyleBtn = document.querySelector(".changestyle").firstElementChild;
+let changeStyleBtn = document.querySelector(".btn-contain-changestyle");
+let btn = document.querySelector(".btn-changestyle");
 let cssPage = document.querySelectorAll("link")[0];
+let logo = document.querySelector("#img-header");
+
 
 let mode = 0;
 
 changeStyleBtn.addEventListener("click", () => {
-    changeStyleBtn.classList.toggle(".clicked-div");
-    changeStyleBtn.firstElementChild.classList.toggle(".clicked");
+    changeStyleBtn.classList.toggle("clicked-div");
+    btn.classList.toggle("clicked");
 
     if (mode === 0) {
         cssPage.href = "./sources/css/darkstyle.css";
+        logo.src = "./photos/logocbgoldcarré.png";
         mode = 1;
     } else {
         cssPage.href = "./sources/css/lightstyle.css";
+        logo.src = "./photos/logocbcarré.png";
         mode = 0;
     };
 });
